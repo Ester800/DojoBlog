7c4a9775bb7e9c86820d132392093608ddef1d35
Christian McKay Stevenson <mckay800@gmail.com>
2021-03-01 18:54:20 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 //import './App.css';
 import Navbar from './NavBar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router> 
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route exact path="/create">
+              <Create />
+            </Route>
+
+            <Route exact path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
